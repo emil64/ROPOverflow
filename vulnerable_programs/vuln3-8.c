779ab43ae1a6e6a4852de7012b261d95e22f92fe
bstelea <bs17580@my.bristol.ac.uk>
2020-12-10 14:55:09 +0200

    add vulnerable program

---
+++ vulnerable_programs/vuln3-8.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+int copyData(char *string)
+{
+	char buf[32];
+	strcpy(buf, string);
+	return (0);
+}
+
+void function2() {
+    int result = 10;
+    printf("Result is %d \n", result);
+    return;
+}
+
+int function(char *string) {
+    int x = 3;
+    switch (x)
+    {
+    case 1:
+        function2();
+        break;
+    
+    default:
+        copyData(string);
+        break;
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	char buffer[700];
+	FILE *file;
+    if (argc !=2)
+    {
+        printf("[*] invalid arguments!\n [*] > %s file_name\n",argv[0]);
+        exit(0);
+    }
+	printf("opening file\n");
+	file = fopen(argv[1],"rb");
+	if (!file)
+	{ 
+		//printf("file not opened %s", strerror(errno));
+		fprintf(stderr,"file not opened %s", strerror(errno));
+		//printf("error");
+		return (0);
+	}
+	printf("file opened\n");
+	fread(buffer, 699,1,file);
+	fclose(file);
+	function(buffer);
+	return (0);
+}
+		
+
