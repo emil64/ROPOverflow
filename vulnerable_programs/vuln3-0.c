0c750dc86ab049c0798311e17f903e804a4de4e1
bstelea <bs17580@my.bristol.ac.uk>
2020-12-10 14:50:26 +0200

    add vulnerable program

---
+++ vulnerable_programs/vuln3-0.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+int copyData(char *string)
+{
+	char buf[32];
+	strcpy(buf, string);
+	return (0);
+}
+
+void recursiveFunction(char *string) {
+    copyData(string);
+    recursiveFunction(string);
+}
+
+int main(int argc, char *argv[])
+{
+	char buffer[700];
+	FILE *file;
+    if (argc !=2)
+    {
+        printf("[*] invalid arguments!\n [*] > %s file_name\n",argv[0]);
+        exit(0);
+    }
+	printf("opening file\n");
+	file = fopen(argv[1],"rb");
+	if (!file)
+	{ 
+		//printf("file not opened %s", strerror(errno));
+		fprintf(stderr,"file not opened %s", strerror(errno));
+		//printf("error");
+		return (0);
+	}
+	printf("file opened\n");
+	fread(buffer, 699,1,file);
+	fclose(file);
+	recursiveFunction(buffer);
+	return (0);
+}
+		
+
